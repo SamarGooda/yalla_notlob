26521876a63573d61a98f00d39c008733c6b7299
ahmed emad <ahmed.emad2051@gmail.com>
2020-04-11 20:12:55 +0200

    add login & register pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  post 'register', to: 'authintication#register'
+  post 'login', to: 'authintication#login'
+  root :to => "authintication#index"
   get '/home' => 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20200411145742_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, default: ""
+      t.string :fname, null:false, default: ""
+      t.string :lname, null:false, default: ""
+      t.string :image, null:false, default: ""
+      t.string :password, null: false, default: ""
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
